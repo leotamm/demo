359931d2d5474a35bfa3c90f9ce47d674f075fd8
Leo Tamm <leo.tamm@gmail.com>
2020-11-09 18:11:45 +0200

    get accountBalance working

--- src/main/java/ee/bcs/valiiit/controller/BankController.java
+++ src/main/java/ee/bcs/valiiit/controller/BankController.java
@@ -14,6 +14,7 @@ public class BankController {
     private Map<String, BigDecimal> account = new HashMap<>();   // paremale keerukam objekt enama infoga
     private Map<String, Account> accounts = new HashMap<>();
     BigDecimal money = new BigDecimal(0);
+    BigDecimal minBalance = new BigDecimal(0);
 
     // createAccount (accountNr)                            CORRECT
     @PostMapping("newAccount")
@@ -30,7 +31,7 @@ public class BankController {
     }
 
     // depositMoney (accountNr, money)                      CORRECT
-    @PutMapping("depositToAccount/{id1}/")
+    @PutMapping("depositToAccount/{id1}")
     public String depositMoney(@PathVariable("id1") String accountNr,
                                @RequestParam("money") BigDecimal money) {
         Account account = accounts.get(accountNr);
@@ -43,7 +44,7 @@ public class BankController {
     }
 
     // withdrawMoney (accountMoney, money))                 CORRECT
-    @PutMapping("withdrawFromAccount/{id1}/")
+    @PutMapping("withdrawFromAccount/{id1}")
     public String withdrawMoney(@PathVariable("id1") String accountNr,
                                 @RequestParam("money") BigDecimal money) {
         Account account = accounts.get(accountNr);
@@ -61,36 +62,48 @@ public class BankController {
         return replyYes + account.getBalance();
     }
 
-    // transferMoney (fromAccount), toAccount, money Hint: RequestBody
-    @PutMapping("transfer/{id1}/{id2}/")
+    // transferMoney (fromAccount), toAccount, money        CORRECT
+    @PutMapping("transfer/{id1}/{id2}")
     public String transferMoney(@PathVariable("id1") String fromAccountNr,
                                 @PathVariable("id2") String toAccountNr,
                                 @RequestParam("money") BigDecimal money) {
 
         Account accountFrom = accounts.get(fromAccountNr);
         BigDecimal oldValue = accountFrom.getBalance();
+        Account toAccount = accounts.get(toAccountNr);
+        BigDecimal oldValue2 = toAccount.getBalance();
+
         if (money.compareTo(oldValue) > 0) {
             String replyNo = "Transfer denied - insufficient funds";
             return replyNo;
         } else {
-            BigDecimal newValue = oldValue.subtract(money); // deduct from one account - works
+            BigDecimal newValue = oldValue.subtract(money); // deducts from one account
             accountFrom.setBalance(newValue);
             accounts.put(fromAccountNr, accountFrom);
 
-            Account toAccount = accounts.get(toAccountNr);  // add to another account - does not add!!!
-            BigDecimal oldValue2 = toAccount.getBalance();
-            BigDecimal newValue2 = oldValue2.add(money);
-            toAccount.setBalance(newValue);
+            BigDecimal newValue2 = oldValue2.add(money);    // adds to another account
+            toAccount.setBalance(newValue2);
             accounts.put(toAccountNr, toAccount);
             String replyYes = "Money transferred, new balance: ";
             return replyYes + toAccount.getBalance();
-
         }
-
     }
 
+    // getAccountBalance (accountNr)                        // CORRECT
+    @GetMapping("checkAccount/{id1}")
+    public String checkBankAccount(@PathVariable("id1") String accountNr) {
+        Account account = accounts.get(accountNr);
+        BigDecimal balance = account.getBalance();
 
-    // getAccountBalance (accountNr)
+        if (balance.compareTo(minBalance) <= 0) {
+            String replyNo = "There are no funds on the account";
+            return replyNo;
+        } else {
+            String replyYes = "Your balance is: ";
+            return replyYes + balance;
+        }
+    }
+
     //
     // createClient(firstName, lastName, .....)
     // muuta createAccount (clientId, accountNr)
