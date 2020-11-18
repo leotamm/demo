8d2eabd7946e605769652c0fe7e03e867f2fc818
Leo Tamm <leo.tamm@gmail.com>
2020-11-18 11:21:17 +0200

    first html efforts

--- src/test/java/ee/bcs/valiiit/AccountServiceTest.java
+++ src/test/java/ee/bcs/valiiit/AccountServiceTest.java
@@ -8,9 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc //need this in Spring Boot test
@@ -22,12 +21,33 @@ public class AccountServiceTest {
 
     @Test
     void registrationWorksThroughAllLayers() throws Exception {
-
         mockMvc.perform(get("/bank/checkAccounts")
                 .contentType("application/json"))
                 .andExpect(status().isOk())
-                //.andExpect(content().string(containsString("EE0001")));
                 .andExpect(content().string(containsString("EE0001")));
+    }
+
+    @Test
+    void inputWorksThroughAllLayers() throws Exception {    //testi urli ja kontrolli, mis vastut tuleb
+        mockMvc.perform(get("/bank/checkAccounts")
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].accountNr").value("EE0002"));
+    }
+
+    @Test
+    void inputWorksThroughAllLayers2() throws Exception {
+        mockMvc.perform(get("/bank/checkAccounts")
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[1].balance").value("10000"));
+    }
+
+    void inputWorksThroughAllLayers3() throws Exception {
+        mockMvc.perform(get("/bank/checkAccount/EE0004")
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("BigDecimal.ZERO")));
     }
 
 }
